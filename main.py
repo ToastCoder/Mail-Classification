d83caf25fc6517a667714435a217ec98bfb1de6a
Vigneshwar Ravichandar <vicky.pcbasic@gmail.com>
2020-11-04 11:06:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+#IMPORTING REQUIRED LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
