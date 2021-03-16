715100fa38fff17fc89491df834f4a91aa538443
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-16 21:59:39 +0530

    Initial code

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-#IMPORTING REQUIRED LIBRARIES
+# IMPORTING REQUIRED LIBRARIES
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import re
+import nltk
+
+# GETTING THE DATA
+dataset = pd.read_csv('data/mailData.csv')
+
+# CLEANING THE TEXT
+nltk.download('stopwords')
+corpus = []
+for i in range(0, 5572):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['EmailText'][i])
+    review = review.lower()
+    review = review.split()
+    ps = nltk.stem.porter.PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(nltk.stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# CREATING BAG OF WORDS MODEL
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn import svm
-from sklearn.model_selection import GridSearchCV
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 0].values
 
+# SPLITTING THE DATASET INTO TEST AND TRAIN SET
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# GETTING DATA
-data = pd.read_csv("data/mailData.csv")
-x = data.iloc[:,0].values
-y = data.iloc[:,-1].values
+# FITTING NAIVR BAYES
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
-# SPLITTING THE DATA INTO TEST DATA AND TRAIN DATA
-x_train,y_train = x[0:4457],y[0:4457]
-x_test,y_test = x[4457:],y[4457:]
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
 
-# EXTRACTING FEATURES
-cv = CountVectorizer()  
-features = cv.fit_transform(x_train)
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
-## BUILDING A MODEL
-tune = {'kernel': ['rbf','linear'], 'gamma': [1e-3, 1e-4],'C': [1, 10, 100, 1000]}
-model = GridSearchCV(svm.SVC(), tune)
-model.fit(features,y_train)
-print(model.best_params_)
-
-# TESTING THE ACCURACY
-print(model.score(cv.transform(x_test),y_test))
