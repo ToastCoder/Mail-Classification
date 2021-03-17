be4bf8aa3b268e1df1fe6fe370f15dc5f2e0b870
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-17 14:40:14 +0530

    Some changes

--- src/train.py
+++ src/train.py
@@ -16,13 +16,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 os.system('cd ..')
 
 # DATA SEGMENTATION
 data = pd.read_csv('data/mailData.csv')
-x = data['EmailText'].values
-y = data['Label'].values
+emails = data['EmailText'].values
+labels = data['Label'].values
+
+# TOKENIZATION OF DATA INPUT
+token = tf.keras.preprocessing.text.Tokenizer(num_words = 5572)
+token.fit_on_texts(emails)
+x = token.texts_to_sequences(emails)
+x = np.array(x)
+x = tf.keras.preprocessing.sequence.pad_sequences(x)
+
 
 
 
