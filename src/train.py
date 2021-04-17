f623dddad7bcdb0327a9a40d4c7ce5de013af7ab
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-17 22:21:34 +0530

    Some changes

---
+++ src/train.py
@@ -0,0 +1,21 @@
+# ////////////////////////////////////////////////////////////
+
+# MAIL CLASSIFICATION
+
+# FILE NAME: train.py
+
+# DEVELOPED BY: Vigneshwar Ravichandar
+
+# TOPICS: Natural Language Processing, TensorFlow , Recurrent Neural Network
+
+# ////////////////////////////////////////////////////////////
+
+# IMPORTING REQUIRED LIBRARIES
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+os.system('cd ..')
