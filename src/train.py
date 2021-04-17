3ff59828bb332ce2bf841f155bea30a6dfad5b8c
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-17 22:29:53 +0530

    Some changes

--- src/train.py
+++ src/train.py
@@ -26,4 +26,22 @@ DATASET_PATH = "data/mailData.csv"
 
 # IMPORTING THE DATASET
 data = pd.read_csv(DATASET_PATH)
+print("Dataset Description:\n",data.describe())
+print("Dataset Head\n",data.head())
+
+# SEGMENTING DATA
+x = data['EmailText'].values
+y = data['Label'].values
+
+# CONVERTING STRING LABELS INTO INTEGER LABELS
+labels = []
+for i in y:
+    if i == 'ham':
+        labels.append(0)
+    if i == 'spam':
+        labels.append(1)
+
+
+
+
 
