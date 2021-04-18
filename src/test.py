1b6bcea82822e743fe5e85108cce34e550d4784d
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-18 10:30:38 +0530

    Some changes

--- src/test.py
+++ src/test.py
@@ -9,3 +9,30 @@
 # TOPICS: Natural Language Processing, TensorFlow , Recurrent Neural Network
 
 # ////////////////////////////////////////////////////////////
+
+# IMPORTING REQUIRED MODULES
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+print(f"TensorFlow version: {tf.__version__}")
+
+DATASET_PATH = "data/mailData.csv"
+MODEL_PATH = './model/spamModel'
+
+# IMPORTING THE DATASET
+data = pd.read_csv(DATASET_PATH)
+print("Dataset Description:\n",data.describe())
+print("Dataset Head:\n",data.head())
+
+# SEGMENTING DATA
+x = data['EmailText'].values
+
+# INITIALIZING TOKENIZER OBJECT AND FITTING TO DATA
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words = 500000)
+tokenizer.fit_on_texts(x)
+
+# OBTAINING WORD INDICES
+indices = tokenizer.word_index
+
+email = input("Enter the received mail: ")
